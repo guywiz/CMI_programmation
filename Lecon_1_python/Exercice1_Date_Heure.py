2cce1c791465a46ec096484b7aa661cf70431014
Guy <guywiz@gmail.com>
2025-02-05 16:05:38 +0100

    pythonisation exercise 1

--- Lecon_1_python/Exercice1_Date_Heure.py
+++ Lecon_1_python/Exercice1_Date_Heure.py
@@ -1,13 +1,13 @@
 import csv
 
-def no_value(row, field):
-	return row[field] == ''
+def no_value(row, fields):
+	return sum([row[field] == '' for field in fields]) > 1
+
+tested_fields = ['Date', 'Heure']
 
 with open('../data/appels_tel.csv', 'r') as csvfile:
 	reader = csv.DictReader(csvfile, delimiter=';')
-	nb_lignes_defectueuses = 0
-	for row in reader:
-		if no_value(row, 'Duration'): # no_value(row, 'Date') or no_value(row, 'Heure'):
-			nb_lignes_defectueuses += 1
+	nb_lignes_defectueuses = sum([no_value(row, tested_fields) for row in reader])
 
-print(f'Le fichier comporte {nb_lignes_defectueuses} lignes dont les champs Date ou heure sont vides')
+print(f"Le fichier comporte {nb_lignes_defectueuses} lignes dont les champs {', '.join(tested_fields)} sont vides")
+
